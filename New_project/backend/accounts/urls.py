5c12175eed011926e42500a6cee8923abecfd9e6
minseong0213 <103939333+minseong0213@users.noreply.github.com>
2023-05-28 01:18:01 +0900

    Delete New_project directory

--- New_project/backend/accounts/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('home/', views.home),
-    path('register/', views.register),
-    path('login/', views.login),
-    path('logout/', views.logout),
-]
