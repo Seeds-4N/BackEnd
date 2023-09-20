5ca4bc9589faec8a680849179a87dccfaa45c511
minseong0213 <minseong0213@naver.com>
2023-09-20 22:37:12 +0900

    test3

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -151,10 +151,11 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CORS_ORIGIN_WHITELIST = (
-     'http://localhost:3000',)
+# CORS_ORIGIN_WHITELIST = (
+#      'http://localhost:3000',)
+
 CORS_ALLOW_CREDENTIALS = True
-SESSION_COOKIE_SAMESITE = None
+# SESSION_COOKIE_SAMESITE = None
 
 
 
