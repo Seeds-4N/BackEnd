629a7321d19f21d2102aa00171d2c521a3b2f613
minseong0213 <minseong0213@naver.com>
2023-09-21 17:17:01 +0900

    fixed whitelist

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -152,7 +152,9 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ORIGIN_WHITELIST = (
-     'http://3.39.142.157:3000',)
+     'http://3.39.142.157:3000',
+     'http://localhost:3000',
+     'http://127.0.0.1:3000')
 CORS_ALLOW_CREDENTIALS = True
 SESSION_COOKIE_SAMESITE = None
 CRSF_COOKIE_SAMESITE = None
